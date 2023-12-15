67c8c9d947e69219d240677a7ffa2d8b3c01a1dc
Vigneshwar <vigneshwarrv666@gmail.com>
2023-12-16 00:00:20 +0530

    added polyfill for map method

---
+++ Polyfills/Polyfills for common JS methods.js
@@ -0,0 +1,49 @@
+/*
+
+Polyfill for Array.map method
+
+*/
+
+Array.prototype.mapPolyfill = function(callback, optionalThisReferer) {
+  var mappedArr = [];
+  if (optionalThisReferer) {
+    callback = callback.bind(optionalThisReferer);
+  }
+  for (let i=0; i<this.length; i++) {
+    mappedArr[i] = (callback(this[i], i, this));
+  }
+  return mappedArr;
+}
+
+let arrCount = 2000;
+let arr = [];
+for(let i=0;i<arrCount;i++) {
+  arr[i] = 0;
+}
+
+const t1 = performance.now();
+arr.map((value, index)=> value + index);
+const t2 = performance.now();
+console.log((t2 - t1) / 1000);
+
+const t3 = performance.now();
+arr.mapPolyfill((value, index)=> value + index);
+const t4 = performance.now();
+console.log((t4 - t3) / 1000);
+
+
+
+/*
+
+ Requirements
+ ------------
+
+  map method always returns the new array without affecting parent array ✅
+  map method takes function as argument to modify the value on each loop of the array. ✅
+  map method recieves two parameters ( callback function ,  optional object to which it should refer ) ✅
+  the callback passed in, will be invoked with three arguments ( value of the loop , index of the loop , array which is being looped ) ✅
+  if second param is not passed, the callback function should always refer to object where it is defined. ( window incase function is defined in toplevel ) ✅
+  if second param is passed, the callback function should refer the passed in object ( arrow functions always refer parent object where its defined ) ✅
+
+
+*/
---
+++ Polyfills/readme.md
@@ -0,0 +1,5 @@
+# Polyfils
+my Learnings and understandings over Javascript pre-defined methods
+
+## why re-invention of wheel ?
+writing polyfills helps me to understand/experience the reasons and logicals behind the actual implementations.
