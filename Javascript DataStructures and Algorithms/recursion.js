2a1ea07c401b3048a3669f3c8996876b98f0bba7
Vigneshwar <vigneshwarrv666@gmail.com>
2023-12-30 17:40:03 +0530

    Added recursion info

---
+++ Javascript DataStructures and Algorithms/recursion.js
@@ -0,0 +1,95 @@
+/*
+
+  Recursion
+  ---------
+
+    function which calls itself again and again to do the task until the end of the task/given conditions met.
+
+    two important things to achieve recursion
+      invocation of the same function with different inputs.
+      function termination when required conditions met.
+
+    Note 🚨
+      failing to invoke the same function for 'n' necassary times would result in incomplete results.
+      failing to add termination condition would lead to infinite loop causing stack limit exceeded error in call stack of javascript engine.
+
+    2 types of recursion functions
+    ------------------------------
+      Pure Recursive function
+      Helper Recursive function
+
+
+    Pure Recursive function
+    -----------------------
+      function used for recursion is a pure function and would always recieve the necessary values via arguments and always return the output
+      does not depend on any external variables outside the function
+
+      Merits
+      ------
+        since functions are pure, their execution is always derivable.
+        when passing non-premitive data type values, only new reference for those values are created instead of new memory allocation ( no value copy )
+
+      Demerits
+      --------
+        when passing primitive data type values, new reference with copy of that value is created ( new memory space ) leading to increased space complexities
+        generally, function invocation will always create new execution context.
+
+    Helper Recusrive function
+    -------------------------
+      function used for this type of recusrion is impure which means it would access variables oustide its recursive function's scope and necessarily need not to return the output.
+      generally consist of two kinds of functions
+        - one function for recursion
+        - one function for wrapping the recursion function and to hold the necessary values required for recursion.
+
+      Merits
+      ------
+        need not to pass all the necessary variables as argument as there is a wrapper function which could hold the values of any data type and its reference in it.
+
+      Demerits
+      --------
+        function might become little complex to understand as they are impure.
+      
+*/
+
+// Example for Pure Recursive Function
+
+  // factorial of given number
+
+function recursivePureFactorial(val, num=1) {
+  if(num >= val) {
+    return val;
+  }
+  return num * recursivePureFactorial(val, num+1);
+}
+
+let result = recursivePureFactorial(6);
+console.log(result);
+
+// Example for Helper Recursive function
+  
+  // factorial of given number
+
+function recursiveHelperFactorial(val) {
+    if (val === 0) {
+        return 0;
+    }
+    
+    let sum = 1;
+    let num = 1;
+
+    function recursiveFactorial() {
+      if (num >= val) {
+        sum *= val;
+        return;
+      }
+      sum *= num;
+      num++;
+      recursiveFactorial();
+    }
+
+    recursiveFactorial();
+    return sum;
+}
+
+let result1 = recursiveHelperFactorial(6);
+console.log(result1);
--- Polyfills/Array.prototype.map - Polyfill.js
+++ Polyfills/Array.prototype.map - Polyfill.js
@@ -4,6 +4,21 @@ Polyfill for Array.map method
 
 */
 
+/*
+
+ Requirements
+ ------------
+
+  map method always returns the new array without affecting parent array ✅
+  map method takes function as argument to modify the value on each loop of the array. ✅
+  map method recieves two parameters ( callback function ,  optional object to which it should refer ) ✅
+  the callback passed in, will be invoked with three arguments ( value of the loop , index of the loop , array which is being looped ) ✅
+  if second param is not passed, the callback function should always refer to object where it is defined. ( window incase function is defined in toplevel ) ✅
+  if second param is passed, the callback function should refer the passed in object ( arrow functions always refer parent object where its defined ) ✅
+
+
+*/
+
 Array.prototype.mapPolyfill = function(callback, optionalThisReferer) {
   var mappedArr = [];
   if (optionalThisReferer) {
@@ -31,30 +46,4 @@ arr.mapPolyfill((value, index)=> value + index);
 const t4 = performance.now();
 console.log((t4 - t3) / 1000);
 
-// [1].map(function(value, index) {
-//   console.log(this);
-//   return value + index;
-// });
-// [1].mapPolyfill(function(value, index) {
-//   console.log(this);
-//  return  value + index
-// });
-
 // console.log(Array.prototype.mapPolyfill.toString());
-
-
-
-/*
-
- Requirements
- ------------
-
-  map method always returns the new array without affecting parent array ✅
-  map method takes function as argument to modify the value on each loop of the array. ✅
-  map method recieves two parameters ( callback function ,  optional object to which it should refer ) ✅
-  the callback passed in, will be invoked with three arguments ( value of the loop , index of the loop , array which is being looped ) ✅
-  if second param is not passed, the callback function should always refer to object where it is defined. ( window incase function is defined in toplevel ) ✅
-  if second param is passed, the callback function should refer the passed in object ( arrow functions always refer parent object where its defined ) ✅
-
-
-*/
